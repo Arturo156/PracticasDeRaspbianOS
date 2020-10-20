ebf1a214f5487123ea17ccdb488838cdc90ec108
Arturo Ernesto Madueño Morales <71353324+Arturo156@users.noreply.github.com>
2020-10-20 14:52:41 -0700

    Create ejercicio1.c

---
+++ practicasCPP/ejercicio1.c
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define PI 3.1416 //dar un valor pi de forma global
+
+int main() 
+{
+ float grados = 15.4; //declaracion de variables
+ float radianes; //declaracion de variables
+ 
+ printf("numero en grados: %f", grados); //mostrar el numero a convertir
+ 
+ radianes = grados*2*PI/360; //operacion para convertir grados a radianes
+ 
+ printf("convertido a radianes son: %f", radianes); //mostrar el numero convertido
+ 
+ 	return 0;
+}
