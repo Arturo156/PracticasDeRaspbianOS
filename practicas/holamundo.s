ed2a278945631441bb61d8b22632d95ee3598cf2
Arturo Ernesto Madueño Morales <71353324+Arturo156@users.noreply.github.com>
2020-10-20 14:33:20 -0700

    Create holamundo.s

---
+++ practicas/holamundo.s
@@ -0,0 +1,11 @@
+root@raspberrypi:/home/pi/revision# ls
+holaMundoAssembler.s
+
+root@raspberrypi:/home/pi/revision# as -o holaMundoAssembler.o holaMundoAssembler.s
+root@raspberrypi:/home/pi/revision# ld -o holaMundoAssembler  holaMundoAssembler.o
+
+root@raspberrypi:/home/pi/revision# ./holaMundoAssembler.s
+
+Hello, world!
+
+root@raspberrypi:/home/pi/revision# 
