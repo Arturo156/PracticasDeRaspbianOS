781549dd7cf466f4788f459ff19153a01477514d
Arturo Ernesto Madueño Morales <71353324+Arturo156@users.noreply.github.com>
2020-10-20 14:55:13 -0700

    Create ejercicio5.c

---
+++ practicasCPP/ejercicio5.c
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float sueldo = 4.50, sueldo2 = 6.75; //declaracion de variables
+    float horas = 40, horas_extras = 13, h, total; //declaracion de variables
+    
+    h = horas + horas_extras; //sumar las horas trabajadas
+    
+    printf("horas trabajadas totales: %f \n", h); //mostrar en pantalla las horas trabajadas
+    
+    total = (sueldo*horas)+(sueldo2*horas_extras); //calcular el total del sueldo
+    
+    printf("sueldo total: %f", total); //mostrar resultado
+    
+	return 0;
+}
