c1d05b6792b32d1e79d1977bdbbaf393b19150cf
Arturo Ernesto Madueño Morales <71353324+Arturo156@users.noreply.github.com>
2020-10-20 14:54:50 -0700

    Create ejercicio4.c

---
+++ practicasCPP/ejercicio4.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sueldo_primer_trabajo = 10, dias = 30, global, global2; //declaracion de variables
+    
+    global = sueldo_primer_trabajo*dias; //calcular el primer sueldo
+    
+    printf("sueldo global del primer trabajo: %i \n", global); //mostrar sueldo del primer trabajo
+    
+     for (int i=1; i<dias; i++) //ciclo for para simular los dias y sumarlos
+    {
+        global2 = global2 + i;
+    }
+    
+    printf("sueldo global del segundo trabajo: %i", global2); //mostrar sueldo del segundo trabajo
+    
+	return 0;
+}
